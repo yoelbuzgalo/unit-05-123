dc3f88066245f4b07ca6bcee3a57949560601fea
Yoel Buzgalo (RIT Student) <ybb5956@rit.edu>
2023-09-29 10:53:30 -0400

    Activity 5.1

---
+++ files.py
@@ -0,0 +1,13 @@
+def print_lines(file_name):
+    '''
+    This function opens a file based on the path/name passed
+    '''
+    my_file = open(file_name)
+    for line in my_file:
+        print(line) # Prints every line in the file
+
+def main():
+    print_lines("data/alice.txt")
+
+if __name__ == "__main__":
+    main()
