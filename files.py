7c00d45a32ffb2cdf9ef1a704e69967056dcc770
Yoel Buzgalo (RIT Student) <ybb5956@rit.edu>
2023-09-29 11:11:33 -0400

    Activity 5.3

--- files.py
+++ files.py
@@ -6,8 +6,19 @@ def print_lines(file_name):
     for line in my_file:
         print(line.strip()) # Prints every line in the file
 
+def word_search(file_name):
+    file = open(file_name)
+    user_input = input("Enter a word that you want to search: ")
+    user_input = user_input.lower()
+    for line in file:
+        line = line.strip()
+        line = line.lower()
+        if line == user_input:
+            return print("Word found")
+    return print("Word not found")
+
 def main():
-    print_lines("data/alice.txt")
+    word_search("data/words.txt")
 
 if __name__ == "__main__":
     main()
