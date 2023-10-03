1dd2268f8ef16396b28fcc17ca5aea6bbb5d2dc0
YoelBuzgalo <yoelbuzgalo@gmail.com>
2023-10-02 20:33:26 -0400

    Assignment 5.1 (Part 4g, 4h and 5)

--- lab_grades.py
+++ lab_grades.py
@@ -40,9 +40,14 @@ def main():
                     print("-----------------------------------------------------------")
                     print("Student","Lab Average",sep="                                         ")
                     print("-----------------------------------------------------------")
-                    
-                    for line in file:
 
+                    # Initialize variables for calculating after iterating every line of grades
+                    class_sum = 0
+                    class_min = 100
+                    class_max = 0
+                    student_counter = 0
+
+                    for line in file:
                         line = line.strip().split(",") # Strips and splits every line after the header
 
                         # Get data associated
@@ -51,13 +56,28 @@ def main():
                         student_grades = line[2:10] # Gets the student's lab grades
                         student_average = student_lab_average(student_grades) # Gets the student's average grade
 
+                        # Adds data to for class average, minimum and max
+                        # Updates the class maximum if student average is higher than latest class maximum (starts with 0 by default)
+                        if student_average > class_max:
+                            class_max = student_average
+                        
+                        # Updates the class minimum if student average is lower than latest class minimum (starts with 100 by default)
+                        if student_average < class_min:
+                            class_min = student_average
+
+                        class_sum += student_average
+                        student_counter += 1
+
                         # Print formatter
                         spaces_count_needed = 48 - (len(student_firstname) + len(student_lastname))
                         spaces = (" " * spaces_count_needed)
 
                         # Prints the result for every average grade calculated
                         print(student_firstname, student_lastname, spaces, student_average)
-                    
+                    print("-----------------------------------------------------------")
+                    print("Class Average", (class_sum/student_counter), sep="                                      ")
+                    print("Class Minimum", class_min, sep="                                      ")
+                    print("Class Maximum", class_max, sep="                                      ")
             except FileNotFoundError:
                 print("No such file:", user_input)
     print("Goodbye!")
