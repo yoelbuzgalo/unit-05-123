ab8261d3bf9ab39f84287011876f532359b1aba2
YoelBuzgalo <yoelbuzgalo@gmail.com>
2023-10-02 20:14:56 -0400

    Assignment 5.1 (Part 4c, 4d, 4e, 4f)

--- lab_grades.py
+++ lab_grades.py
@@ -12,9 +12,11 @@ def student_lab_average(grades):
     This function calculates a student's lab average grades based on the data passed
     '''
     total = 0 # Starts with 0
-    lab_grades = 10 # Defaulted to 10 lab grades, so no matter how many data is passed but must be all 10 grades for average. 
+    lab_grades = 8 # Defaulted to 8 lab grades, so no matter how many data is passed but must be all 8 grades for average. (Note typo in instruction as there is only 8 labs in .csv file) 
     for grade in grades:
-        total += grade # Add to total for every grade received
+        if grade == '':
+            continue # Skips on empty strings of an expected lab grade (if it doesnt exist)
+        total += float(grade) # Add to total for every grade received
     return (total/lab_grades) # Calculates the average based on default 10 classes, so even if it was missing 1 or more classes, it would count them as 0
 
 def main():
@@ -30,7 +32,34 @@ def main():
         elif user_input == '':
             continue # Goes back to initial loop
         else:
-            print("Testing!")
+            try:
+                with open(user_input) as file:
+
+                    header = next(file).strip().split(",") # Gets the header of the file, and strips all the whitespace + splits with comma
+                    print("Lab Averages from:", user_input)
+                    print("-----------------------------------------------------------")
+                    print("Student","Lab Average",sep="                                         ")
+                    print("-----------------------------------------------------------")
+                    
+                    for line in file:
+
+                        line = line.strip().split(",") # Strips and splits every line after the header
+
+                        # Get data associated
+                        student_lastname = line[0] # Gets the student's last name
+                        student_firstname = line[1] # Gets the student's first name
+                        student_grades = line[2:10] # Gets the student's lab grades
+                        student_average = student_lab_average(student_grades) # Gets the student's average grade
+
+                        # Print formatter
+                        spaces_count_needed = 48 - (len(student_firstname) + len(student_lastname))
+                        spaces = (" " * spaces_count_needed)
+
+                        # Prints the result for every average grade calculated
+                        print(student_firstname, student_lastname, spaces, student_average)
+                    
+            except FileNotFoundError:
+                print("No such file:", user_input)
     print("Goodbye!")
     return
 
