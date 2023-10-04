96149bb21da8afa9509453202085c009f7b1a2ac
Yoel Buzgalo (RIT Student) <ybb5956@rit.edu>
2023-10-04 10:29:20 -0400

    Activity 5.24

--- csv_fun.py
+++ csv_fun.py
@@ -10,10 +10,10 @@ def opener(filename):
 
 def names_and_addresses(filename):
     with open(filename) as file:
-        header = next(file).strip().split(",")
-        for line in file:
-            values = line.strip().split(",")
-            print("Name:", values[0], "Address:", values[1])
+        csv_reader = csv.reader(file)
+        header = next(csv_reader)
+        for record in csv_reader:
+            print("Name:", record[0], "Address:", record[1])
 
 def first_only(filename):
     with open(filename) as file:
@@ -27,8 +27,8 @@ def first_only(filename):
 def main():
     # file_input = input("Enter filename to open: ")
     # print("File is able to be opened: ", opener("data/"+file_input))
-    # names_and_addresses("data/full_grades_010.csv")
-    print(first_only("data/full_grades_010.csv"))
+    names_and_addresses("data/full_grades_010.csv")
+    # print(first_only("data/full_grades_010.csv"))
 
 if __name__ == "__main__":
     main()
