172457fd94961cbfd040da422c7fbc3b31e274ee
YoelBuzgalo <yoelbuzgalo@gmail.com>
2023-10-06 00:24:23 -0400

    Assignment 5.2 (Part 2a)

---
+++ advanced_plots.py
@@ -0,0 +1,20 @@
+import csv
+import plotter
+import re
+
+def plot_grades(filename, firstname, lastname):
+    with open(filename) as file:
+        csv_reader = csv.reader(file)
+        next(csv_reader)
+        first_name_pattern = firstname + ", [A-Za-z]*"
+        last_name_pattern = ", " + lastname
+        for record in csv_reader:
+            if re.findall(first_name_pattern,record[0]) and re.findall(last_name_pattern, record[0]):
+                plotter.init(record[0], "Grade Item", "Score")
+                plotter.new_series("Series 1")
+                plotter.plot()
+        else:
+            print("Could not find any matched result, aborting")
+        
+
+plot_grades("data/full_grades_010.csv", "Tsiatsos", "Shamella")
