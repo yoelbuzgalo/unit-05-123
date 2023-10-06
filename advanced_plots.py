966c73f476cb69da779cc8cd5c6317597250e5c9
YoelBuzgalo <yoelbuzgalo@gmail.com>
2023-10-06 00:41:04 -0400

    Assignment 5.2 (Part 3)

--- advanced_plots.py
+++ advanced_plots.py
@@ -18,6 +18,22 @@ def plot_grades(filename, firstname, lastname):
                 plotter.plot()
         else:
             print("Could not find any matched result, aborting")
+
+def get_average(filename, column):
+    with open(filename) as file:
+        csv_reader =csv.reader(file)
+        next(csv_reader)
+        total_score = 0
+        counter = 0
+        for record in csv_reader:
+            counter += 1
+            try:
+                total_score += float(record[column])
+            except:
+                print("Not able to convert", record[column], " to a float value...skipping")
+                continue
+        print("Average of a given column is: ", (total_score/counter))
         
 
 plot_grades("data/full_grades_010.csv", "Tsiatsos", "Shamella")
+get_average("data/full_grades_010.csv", 4)
